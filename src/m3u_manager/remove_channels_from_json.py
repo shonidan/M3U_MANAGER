3aeb8c330e68d04a199c0f1fa14f269dfd6571b2
shonidan <38263591+shonidan@users.noreply.github.com>
2024-04-11 11:34:02 -0300

    Update remove_channels_from_json.py

--- src/m3u_manager/remove_channels_from_json.py
+++ src/m3u_manager/remove_channels_from_json.py
@@ -1,5 +1,4 @@
 import json
-import re
 
 
 def remove_channels_from_json(json_file, substrings):
@@ -29,7 +28,7 @@ def remove_channels_from_json(json_file, substrings):
 
         for channel in data[key]:
             for substring in substrings:
-                if substring.lower() in channel['info'].lower() or substring.lower() in channel['url'].lower():
+                if substring.lower() in channel['info'].lower():
                     channels_to_remove.append(channel)
                     break  # No need to check further once a match is found
 
@@ -40,6 +39,46 @@ def remove_channels_from_json(json_file, substrings):
     # Remove the keys and their associated channels
     for key in keys_to_remove:
         del data[key]
+
+    # Write the updated dictionary back to the JSON file
+    with open(json_file, 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+
+
+def keep_channels_with_substrings(json_file, substrings):
+    """
+    Keeps channels containing any of the specified substrings in their info or url fields.
+
+    Args:
+        json_file (str): Path to the JSON file.
+        substrings (list): List of substrings to search for in channel info or url fields.
+    """
+    # Open the JSON file and load its content into a dictionary
+    with open(json_file, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    # Iterate over the dictionary keys
+    keys_to_keep = []
+
+    for key in data:
+        # Check if any of the channels within the group match the substrings
+        channels_to_keep = []
+
+        for channel in data[key]:
+            for substring in substrings:
+                if substring.lower() in channel['info'].lower() or substring.lower() in channel['url'].lower():
+                    channels_to_keep.append(channel)
+                    break  # No need to check further once a match is found
+
+        # Remove channels not containing any of the substrings
+        data[key] = channels_to_keep
+        if channels_to_keep:
+            keys_to_keep.append(key)
+
+    # Remove keys without any channels to keep
+    for key in list(data.keys()):
+        if key not in keys_to_keep:
+            del data[key]
 
     # Write the updated dictionary back to the JSON file
     with open(json_file, 'w', encoding='utf-8') as file:
