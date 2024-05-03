2705a66cc20d168a4367012a0739b9a3bf4357b4
shonidan <38263591+shonidan@users.noreply.github.com>
2024-05-03 13:39:19 -0300

    Update fix_json.py

--- src/m3u_manager/fix_json.py
+++ src/m3u_manager/fix_json.py
@@ -13,12 +13,36 @@ def fix_json(file_name):
 
         return new_data
 
+    def remove_intermediate_characters(data):
+        new_data = {}  # Nuevo diccionario con las claves modificadas
+        for group_name, channels in data.items():
+            # Reemplazar caracteres intermedios no deseados entre palabras
+            new_group_name = re.sub(r'[^a-zA-Z0-9/-_:]+', '_', group_name)
+            new_data[new_group_name] = channels
+
+        return new_data
+
+    def remove_extra_underscores(data):
+        new_data = {}  # Nuevo diccionario con las claves modificadas
+        for group_name, channels in data.items():
+            # Reemplazar múltiples guiones bajos con solo uno
+            new_group_name = re.sub(r'_+', '_', group_name)
+            new_data[new_group_name] = channels
+
+        return new_data
+
     # Leer el JSON desde el archivo
     with open(file_name, 'r', encoding='utf-8') as file:
         data = json.load(file)
 
     # Modificar las claves del diccionario
     modified_data = remove_special_characters_group_names(data)
+
+    # Aplicar la función para eliminar caracteres intermedios no deseados
+    modified_data = remove_intermediate_characters(modified_data)
+
+    # Aplicar la función para eliminar guiones bajos extras
+    modified_data = remove_extra_underscores(modified_data)
 
     # Ordenar el diccionario por keys (group-title) alfabéticamente
     sorted_data = dict(sorted(modified_data.items()))
