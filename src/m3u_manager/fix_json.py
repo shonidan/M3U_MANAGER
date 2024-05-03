9fd6203924d479de71193c823f9f8c9087faaf0d
shonidan <38263591+shonidan@users.noreply.github.com>
2024-05-03 13:08:53 -0300

    Update fix_json.py

--- src/m3u_manager/fix_json.py
+++ src/m3u_manager/fix_json.py
@@ -1,58 +1,28 @@
 import json
-
+import re
 
 def fix_json(file_name):
-    def process_data(data):
+    def remove_special_characters_group_names(data):
+        new_data = {}  # Nuevo diccionario con las claves modificadas
         for group_name, channels in data.items():
-            for channel in channels:
-                if 'group-title=\"\",' in channel['info']:
-                    channel['info'] = channel['info'].replace('group-title=\"\",', f'group-title=\"{group_name}\",')
-                elif 'group-title=\" ' in channel['info']:
-                    channel['info'] = channel['info'].replace('group-title=\"', f'group-title=\"{group_name}\",')
+            # Eliminar caracteres especiales al principio del nombre del grupo
+            new_group_name = re.sub(r'^[^a-zA-Z0-9]+', '', group_name)
+            # Eliminar caracteres especiales al final del nombre del grupo
+            new_group_name = re.sub(r'[^a-zA-Z0-9]+$', '', new_group_name)
+            new_data[new_group_name] = channels
 
-    def remove_asterisk_group_names(data):
-        while any(group_name.startswith("*") for group_name in data.keys()):
-            for group_name in list(data.keys()):
-                if group_name.startswith("*"):
-                    new_group_name = group_name.replace("*", "", 1)
-                    data[new_group_name] = data.pop(group_name)
-    def remove_underscore_group_names(data):
-        while any(group_name.startswith("_") for group_name in data.keys()):
-            for group_name in list(data.keys()):
-                if group_name.startswith("_"):
-                    new_group_name = group_name.replace("_", "", 1)
-                    data[new_group_name] = data.pop(group_name)
-
-    def remove_pipe_group_names(data):
-        while any(group_name.startswith("|") for group_name in data.keys()):
-            for group_name in list(data.keys()):
-                if group_name.startswith("|"):
-                    new_group_name = group_name.replace("|", "", 1)
-                    data[new_group_name] = data.pop(group_name)
+        return new_data
 
     # Leer el JSON desde el archivo
     with open(file_name, 'r', encoding='utf-8') as file:
         data = json.load(file)
 
-    # Llamar a la función interna para borrar guion bajo del grupo
-    remove_asterisk_group_names(data)
-
-    # Llamar a la función interna para borrar guion bajo del grupo
-    remove_underscore_group_names(data)
-
-    # Llamar a la función interna para borrar los pipe del grupo
-    remove_pipe_group_names(data)
-
-    # Llamar a la función interna para borrar guion bajo del grupo por segunda vez
-    remove_underscore_group_names(data)
-
-    # Llamar a la función interna para procesar los datos
-    process_data(data)
+    # Modificar las claves del diccionario
+    modified_data = remove_special_characters_group_names(data)
 
     # Ordenar el diccionario por keys (group-title) alfabéticamente
-    sorted_data = dict(sorted(data.items()))
+    sorted_data = dict(sorted(modified_data.items()))
 
     # Escribir el JSON actualizado de nuevo al archivo
     with open('ALL_CHANNELS.json', 'w', encoding='utf-8') as file:
         json.dump(sorted_data, file, indent=4)
-
